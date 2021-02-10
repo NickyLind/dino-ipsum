88841b289eebe87a4db28a9583507549de665b45
Dani Renner <danijrenner@gmail.com>
2021-02-09 17:11:45 -0800

    updates
    Co-authored by: Nick Lindau <nicholaithegreat@gmail.com>

--- docs/main.js
+++ docs/main.js
@@ -2,9 +2,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-let url = `http://dinoipsum.herokuapp.com/api/?format=json`;
-
 function Words(input) {
+let url = `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1`;
 input = parseInt(input);
 url = url+"&words="+input;
 return url;
@@ -29,26 +28,3 @@ $(document).ready(function() {
     }
   });
 });
-
-// $(document).ready(function() {
-//   $("#trending").click(function()  {
-//     $('.output').empty();
-//     let request = new XMLHttpRequest();
-//     const url = `https://api.giphy.com/v1/gifs/trending?&api_key=${process.env.API_KEY}&limit=5`;
-
-//     request.onreadystatechange = function() {
-//       if (this.readyState === 4 && this.status === 200) {
-//         const response = JSON.parse(this.responseText);
-//         getElements(response);
-//       }
-//     };
-//     request.open("GET", url, true);
-//     request.send();
-
-//     function getElements(response)  {
-//       const myArray = [0,1,2,3,4];
-//       for (let i = 0; i < myArray.length; i++)  {
-//         $('.output').append(`<img width="300" height="300" src="${response.data[i].images.original.url}">`);
-//       }
-//     }
-//   });
